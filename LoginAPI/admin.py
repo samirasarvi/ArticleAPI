3052d377acd18b4478f97574937bbf711a318c01
samirasarvi <samira.sarvi20@gmail.com>
2021-02-26 20:06:43 +0330

    'LoginAPIModels'

--- LoginAPI/admin.py
+++ LoginAPI/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+@admin.register(Article)
+class ArticleModel(admin.ModelAdmin):
+    list_filter = ('title','descripton')
+    list_display = ('title','descripton')
